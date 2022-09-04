1f4a9af0bfba70be8937a252b585d56331ee87e1
zhanziyang <zhanziyang126@hotmail.com>
2022-09-05 02:48:02 +0800

    .

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import adapter from 'webrtc-adapter';
+import 'image-capture';
 import { createApp } from 'vue';
 import App from './App.vue';
 import '@/style/index.css';
--- src/views/test.vue
+++ src/views/test.vue
@@ -11,21 +11,50 @@
   <canvas
     id="canvaselement"
     width="1920"
-    height="1080"
   ></canvas>
 </template>
 
 <script setup>
 import { onMounted } from 'vue';
 
+let videoDevice;
 onMounted(() => {
   const videoElement = document.querySelector('video');
-  console.log(videoElement);
+  const canvas = document.querySelector('canvas');
+  var ctx = canvas.getContext('2d', { alpha: false });
+
   const h = (stream) => {
-    console.log(stream);
-    'srcObject' in videoElement ? ((videoElement.src = ''), (videoElement.srcObject = stream)) : (videoElement.src = URL.createObjectURL(stream));
-    videoElement.play();
+    // console.log(stream);
+    // 'srcObject' in videoElement ? ((videoElement.src = ''), (videoElement.srcObject = stream)) : (videoElement.src = URL.createObjectURL(stream));
+    // videoElement.play();
+
+    videoDevice = stream.getVideoTracks()[0];
+    // Check if this device supports a picture mode...
+    let captureDevice = new ImageCapture(videoDevice);
+    if (captureDevice) {
+      setInterval(() => {
+        captureDevice
+          .grabFrame()
+          .then(processFrame)
+          .catch(() => {});
+      }, 1000 / 60);
+    }
   };
+
+  function processFrame(imageBitmap) {
+    canvas.width = window.innerWidth * window.devicePixelRatio;
+    canvas.height = (imageBitmap.height / imageBitmap.width) * window.innerWidth * window.devicePixelRatio;
+    canvas.style.width = canvas.width / window.devicePixelRatio + 'px';
+    canvas.style.height = canvas.height / window.devicePixelRatio + 'px';
+    ctx.drawImage(imageBitmap, 0, 0, canvas.width, canvas.height);
+  }
+
+  videoElement.addEventListener('loadedmetadata', () => {
+    canvas.height = (videoElement.videoHeight / videoElement.videoWidth) * window.innerWidth * window.devicePixelRatio;
+    canvas.width = window.innerWidth * window.devicePixelRatio;
+    canvas.style.width = canvas.width / window.devicePixelRatio + 'px';
+    canvas.style.height = canvas.height / window.devicePixelRatio + 'px';
+  });
   // var s = {
   //   audio: true,
   //   video: { width: 1920, height: 1080 },
@@ -45,19 +74,14 @@ onMounted(() => {
     ? navigator.getUserMedia(s, h, d)
     : a(new Error('当前浏览器不支持打开摄像头'));
 
-  const canvas = document.querySelector('canvas');
-  canvas.height = window.innerHeight;
-  canvas.width = window.innerWidth;
-  var ctx = canvas.getContext('2d', { alpha: false });
+  // function drawImage(video) {
+  //   // ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+  // }
+  // var fps = 60;
 
-  function drawImage(video) {
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-  }
-  var fps = 60;
-
-  const canvasInterval = window.setInterval(() => {
-    drawImage(videoElement);
-  }, 1000 / fps);
+  // const canvasInterval = window.setInterval(() => {
+  //   drawImage(videoElement);
+  // }, 1000 / fps);
 });
 </script>
 
