161b39082f825ae9add44214f452404facfe1e29
zhanziyang <zhanziyang126@hotmail.com>
2022-09-05 05:09:15 +0800

    rollback

--- src/views/test.vue
+++ src/views/test.vue
@@ -17,37 +17,15 @@
 <script setup>
 import { onMounted } from 'vue';
 
-let videoDevice;
 onMounted(() => {
   const videoElement = document.querySelector('video');
   const canvas = document.querySelector('canvas');
-  var ctx = canvas.getContext('2d', { alpha: false });
 
   const h = (stream) => {
-    // console.log(stream);
-    // 'srcObject' in videoElement ? ((videoElement.src = ''), (videoElement.srcObject = stream)) : (videoElement.src = URL.createObjectURL(stream));
-    // videoElement.play();
-
-    videoDevice = stream.getVideoTracks()[0];
-    // Check if this device supports a picture mode...
-    let captureDevice = new ImageCapture(videoDevice);
-    if (captureDevice) {
-      setInterval(() => {
-        captureDevice
-          .grabFrame()
-          .then(processFrame)
-          .catch(() => {});
-      }, 1000 / 60);
-    }
+    console.log(stream);
+    'srcObject' in videoElement ? ((videoElement.src = ''), (videoElement.srcObject = stream)) : (videoElement.src = URL.createObjectURL(stream));
+    videoElement.play();
   };
-
-  function processFrame(imageBitmap) {
-    canvas.width = window.innerWidth * window.devicePixelRatio;
-    canvas.height = (imageBitmap.height / imageBitmap.width) * window.innerWidth * window.devicePixelRatio;
-    canvas.style.width = canvas.width / window.devicePixelRatio + 'px';
-    canvas.style.height = canvas.height / window.devicePixelRatio + 'px';
-    ctx.drawImage(imageBitmap, 0, 0, canvas.width, canvas.height);
-  }
 
   videoElement.addEventListener('loadedmetadata', () => {
     canvas.height = (videoElement.videoHeight / videoElement.videoWidth) * window.innerWidth * window.devicePixelRatio;
@@ -74,14 +52,16 @@ onMounted(() => {
     ? navigator.getUserMedia(s, h, d)
     : a(new Error('当前浏览器不支持打开摄像头'));
 
-  // function drawImage(video) {
-  //   // ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-  // }
-  // var fps = 60;
+  var ctx = canvas.getContext('2d', { alpha: false });
 
-  // const canvasInterval = window.setInterval(() => {
-  //   drawImage(videoElement);
-  // }, 1000 / fps);
+  function drawImage(video) {
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+  }
+  var fps = 60;
+
+  const canvasInterval = window.setInterval(() => {
+    drawImage(videoElement);
+  }, 1000 / fps);
 });
 </script>
 
